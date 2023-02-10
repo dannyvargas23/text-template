fb21eefcefc5ba3fdf8980534bd8fdce28b5dfe8
Danny Vargas <danny@ziggletech.com>
2023-02-10 14:45:25 -0500

    Create options.d.ts

---
+++ interfaces/options.d.ts
@@ -0,0 +1,8 @@
+export interface TextTemplateOptions {
+    start?: string;
+    end?: string;
+    language?: 'html' | 'readme' | 'pascal' | 'forth' | 'haskell' | '';
+    behavior?: '' | 'slot';
+    removeOnError?: boolean;
+    delimiter?: string;
+}
